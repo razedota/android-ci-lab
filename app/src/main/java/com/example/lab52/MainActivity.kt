1ef6d1128d31aaa7d274cb58f0db6ba728526fe9
razedota <konstantinkrasnoselskiy@gmail.com>
2026-05-25 18:10:08 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lab52/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.example.lab52
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.lab52.ui.theme.Lab52Theme
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
+
+        setContent {
+            Lab52Theme {
+
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = Color.Cyan
+                ) {
+
+                    Greeting(
+                        name = "Костянтин Красносельський "
+                    )
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+
+    Text(
+        text = "Hi, my name is $name"
+    )
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+
+    Lab52Theme {
+
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = Color.Cyan
+        ) {
+
+            Greeting(
+                name = "Красносельський Костянтин"
+            )
+
+        }
+    }
+}
